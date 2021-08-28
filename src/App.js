78fcf591d0fb7d2daebad553386efccec4891031
Vitalii <vitaliimelnykov@gmail.com>
2021-08-28 20:07:15 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './components/post';
+import { useSelector } from 'react-redux';
+import AddPost from './components/addPost';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const posts = useSelector((state) => state.posts);
+	return (
+		<div className="App">
+			<AddPost />
+			{posts.map((post) => (
+				<Post {...post} key={post.id} />
+			))}
+		</div>
+	);
 }
 
 export default App;
