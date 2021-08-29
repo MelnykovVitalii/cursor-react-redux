29598f64c577086b66ec3a09e6692d229fcfc2e6
Vitalii <vitaliimelnykov@gmail.com>
2021-08-29 22:24:03 +0300

    fix formatter

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { useSelector } from 'react-redux';
 import AddPost from './components/addPost';
 
 function App() {
-	const posts = useSelector((state) => state.posts);
-	return (
-		<div className="App">
-			<AddPost />
-			{posts.map((post) => (
-				<Post {...post} key={post.id} />
-			))}
-		</div>
-	);
+  const posts = useSelector((state) => state.posts);
+  return (
+    <div className="App">
+      <AddPost />
+      {posts.map((post) => (
+        <Post {...post} key={post.id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
